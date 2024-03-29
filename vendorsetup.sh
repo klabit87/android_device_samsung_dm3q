8550426d06a8d0d4f66ece65d3166b7b1cfca51f
klabit87 <11029568+klabit87@users.noreply.github.com>
2024-03-28 20:35:16 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_dm3q-user
-add_lunch_combo omni_dm3q-userdebug
-add_lunch_combo omni_dm3q-eng
+add_lunch_combo twrp_dm3q-user
+add_lunch_combo twrp_dm3q-userdebug
+add_lunch_combo twrp_dm3q-eng
